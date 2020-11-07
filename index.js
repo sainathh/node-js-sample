8ed2ad5c2d2b0565f83266ed3326c81affca7d3b
sainathh <51146038+sainathh@users.noreply.github.com>
2020-11-07 12:34:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Nice to See You')
+  response.send('Hello DevOps! Nicee to See You')
 })
 
 app.listen(app.get('port'), function() {
